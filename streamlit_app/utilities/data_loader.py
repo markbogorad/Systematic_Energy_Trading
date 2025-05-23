9fb6580912c23533e635651ea6ccbac6f5c77a1c
markbogorad <mabogorad@gmail.com>
2025-05-23 16:11:20 -0400

    loading from drive

--- streamlit_app/utilities/data_loader.py
+++ streamlit_app/utilities/data_loader.py
@@ -1,6 +1,35 @@
 import sqlite3
 import pandas as pd
 
+def download_file_from_google_drive(id, destination):
+    import requests
+
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+    response = session.get(URL, params={"id": id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+    return None
+
+
+def save_response_content(response, destination, chunk_size=32768):
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(chunk_size):
+            if chunk:  # filter out keep-alive chunks
+                f.write(chunk)
+
 def load_commodity_data(db_path):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
